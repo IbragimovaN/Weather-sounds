bb568cdbf10172871b9d963dcc0ef53a4e3639c5
Anastasiya <anastasiya.ibr@mail.ru>
2024-10-25 16:16:12 +0300

    feat: готовы кнопки, переключение картинок

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
 import "./index.scss";
+import sun from "./assets/icons/sun.svg";
+import rain from "./assets/icons/cloud-rain.svg";
+import snow from "./assets/icons/cloud-snow.svg";
+
+console.log(sun);
 const body = document.querySelector("body");
+const h1 = document.querySelector("h1");
+const btnWrapper = document.createElement("div");
+btnWrapper.className = "btnWrapper";
+body.appendChild(btnWrapper);
 
-const newParagraph = document.createElement("p");
-newParagraph.className = "text";
-newParagraph.dataset.id = "3";
-newParagraph.textContent = "Третий абзац";
+const clickFunc = (type) => {
+  body.className = type;
+  h1.className = `${type}Color`;
+};
 
-body.append(newParagraph);
+const createButton = (type, icon) => {
+  const newBtn = document.createElement("button");
+  newBtn.className = type;
+  newBtn.id = type;
+  const image = document.createElement("img");
+  image.src = icon;
+  newBtn.appendChild(image);
+  btnWrapper.appendChild(newBtn);
+};
+
+document.addEventListener("click", (event) => {
+  console.log(event.target.id);
+  clickFunc(event.target.id);
+});
+
+createButton("rainy", rain);
+createButton("summer", sun);
+createButton("winter", snow);
